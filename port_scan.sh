a63d622cd0c5b852e87ce835623f137d7aad8784
gwillgues <96144967+gwillgues@users.noreply.github.com>
2022-06-03 21:42:41 -0500

    Update port_scan.sh

--- port_scan.sh
+++ port_scan.sh
@@ -1,1 +1,1 @@
-for i in {1..1024};do (echo </dev/tcp/192.168.2.1/$i) &>/dev/null && echo -e "$i -- OPEN";done
+for i in {1..1024};do (echo </dev/tcp/192.168.0.1/$i) &>/dev/null && echo -e "$i -- OPEN";done
